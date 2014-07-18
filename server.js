7b46ab0ba826255a6ba9212f9c9a71dbe3e7f97a
briankostar <brian.kostar@gmail.com>
2014-07-18 11:06:51 +0000

    routers

--- server.js
+++ server.js
@@ -12,17 +12,45 @@ app.use(bodyParser());
 var port = process.env.PORT || 8080;
 
 //ROUTES for our API
-//instance of the express router
+//instance of the express router. we can make multiple instances to handel authentication, api, etc
 var router = express.Router();
+
+//route middleware that occurs for all request
+app.use(function(req, res, next){
+    console.log(req.method, req.url);
+    //continue doing what we were doing
+    next();
+});
+
+//param middleware to check 'name' parameter
+router.param('name', function(req, res, next, name){
+    //logic to check name validation
+    if(name == "brian"){
+	req.name = "you majestic beast";
+    }else{
+	req.name = name;
+    }
+    console.log('doing name validations on the parameter value : ' + name);
+    next();
+})
 
 //test route
 router.get('/', function(req, res){
     res.json({message: "yes! this works!"});
 });
+router.get('/test', function(req, res){
+    res.json({message: "testing api!"});
+});
+router.get('/hello/:name', function(req,res){
+    res.send('hello ' + req.name + '!');
+});
 
 //REGISTER OUR ROUTES
 //First endpoint of our api will be .... /api
-app.use('/api', router);
+//this sets the ROOT router. /app /app/test
+app.use('/', router);
+
+app.route('/login').get(function(req, res){res.send('login form')});
 
 //start the app
 app.listen(port);
